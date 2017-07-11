627e26a3710f1dfc5a0c584781bcecad7c258039
yinxingxue <allen_smiss@hotmail.com>
2017-07-11 10:57:58 +0800

    new

---
+++ cplex/matrix/cplex/tsl/ntu/sg/src/matrix/IllegalDimensionException.java
@@ -0,0 +1,15 @@
+package matrix.cplex.tsl.ntu.sg.src.matrix;
+
+public class IllegalDimensionException extends Exception {
+
+	public IllegalDimensionException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public IllegalDimensionException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
